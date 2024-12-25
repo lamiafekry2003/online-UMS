f84e3296161968dc57b4e3d886c5b84457327d72
lamia fekry <123817607+lamiafekry2003@users.noreply.github.com>
2024-12-25 09:45:48 +0200

    final

--- src/Component/MasterLayout/MasterLayout.tsx
+++ src/Component/MasterLayout/MasterLayout.tsx
@@ -3,21 +3,22 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import SideBar from "../SideBar/SideBar";
-// grid grid-cols-[.1fr_6fr]
+
+
 
 export default function MasterLayout() {
   return (
     <div className=" grid grid-cols-[.1fr_6fr] h-full">
-      {/* Sidebar */}
+      
       <div className="">
        <SideBar/>
       </div>
 
-      {/* Main Content */}
+      
       <div className="flex flex-col">
-        {/* Navbar */}
+        
         <Navbar />
-        {/* Outlet */}
+        
         <div className="flex-1 overflow-y-auto">
           <Outlet />
         </div>
